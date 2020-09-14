183fc0634af87a0470edcf567883c2e6c22ad00f
Jason Yellick <jyellick@us.ibm.com>
2020-09-14 15:04:32 -0400

    Mark as deprecated, and remove code
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p output
-
-for ii in $(seq 0 3) ; do
-	./mir-sample --cryptoConfig cryptogen/config${ii}.yaml --eventLog output/${ii}.eventlog --msgsPerClient 10000 &> output/${ii}.log &
-done
